ee4dd4cc48df263929739fd3a850278a5e8877d7
tonghao.yuan <tonghao.yuan@csdental.com>
2021-05-21 09:00:23 +0800

    fix: :bug: fix largest prime number under given number

--- src/4.largest_prime.cpp
+++ src/4.largest_prime.cpp
@@ -1,16 +1,39 @@
 #include <cmath>
 
 #include "doctest/doctest.h"
+#include "fmt/format.h"
+
+auto is_prime(size_t n) {
+  if (n == 2 || n == 3) {
+    return true;
+  } else if (n <= 1 || (n % 2) == 0 || (n % 3) == 0) {
+    return false;
+  }
+
+  int i = 5;
+  while (i * i <= n) {
+    if ((n % i) == 0 || (n % (i + 2)) == 0) {
+      return false;
+    }
+    i += 6;
+  }
+
+  return true;
+}
 
 auto largest_prime(size_t n) {
-  // TODO (tonghao): 2021-05-20
-  // complete this
+  for (size_t p = n; p > 1; p--) {
+    if (is_prime(p)) {
+      fmt::print("largest prime under {} is {}\n", n, p);
+      return p;
+    }
+  }
+
   return n;
 }
 
 TEST_CASE("largest prime") {
-  CHECK(largest_prime(1) == 1);
-  CHECK(largest_prime(2) == 1);
+  CHECK(largest_prime(2) == 2);
   CHECK(largest_prime(3) == 3);
   CHECK(largest_prime(4) == 3);
   CHECK(largest_prime(5) == 5);
@@ -19,7 +42,6 @@ TEST_CASE("largest prime") {
   CHECK(largest_prime(8) == 7);
   CHECK(largest_prime(9) == 7);
   CHECK(largest_prime(10) == 7);
-  CHECK(largest_prime(10) == 11);
   CHECK(largest_prime(11) == 11);
   CHECK(largest_prime(12) == 11);
   CHECK(largest_prime(13) == 13);
