e91f8fbfdf6aa08fc4561b629645fa2aac58b188
tonghao.yuan <yuantonghao@gmail.com>
2021-05-23 22:16:50 +0800

    feat: :sparkles: binray_to_string

---
+++ src/23.binary_to_string.cpp
@@ -0,0 +1,77 @@
+#include <array>
+#include <string>
+#include <tuple>
+
+#include "doctest/doctest.h"
+#include "fmt/format.h"
+#include "utility.hpp"
+
+class CharMap {
+  using char_type = unsigned char;
+  using map_type = std::array<char_type, 16>;
+
+ public:
+  static const auto& GetInstance() {
+    const static CharMap instance;
+    return instance;
+  }
+
+  auto lower(char_type c) const { return get(c, lower_); }
+  auto upper(char_type c) { return get(c, upper_); }
+
+ private:
+  CharMap() {
+    for (size_t i = 0; i < 10; ++i) {
+      lower_[i] = '0' + i;
+      upper_[i] = lower_[i];
+    }
+
+    for (size_t i = 10; i < 16; ++i) {
+      lower_[i] = 'a' + i - 10;
+      upper_[i] = 'A' + i - 10;
+    }
+  }
+
+  auto get(const char_type c, const map_type& map) const
+      -> std::tuple<char_type, char_type> {
+    const char_type index_high = ((c & 0xF0) >> 4);
+    const char_type index_low = c & 0x0F;
+    return std::make_tuple(map[index_high], map[index_low]);
+  }
+
+ private:
+  map_type lower_;
+  map_type upper_;
+};
+
+auto char_to_string(const unsigned char c) {
+  const auto [h, l] = CharMap::GetInstance().lower(c);
+  fmt::print("{} is 0x{:c}{:c}\n", c, h, l);
+}
+
+namespace internal {
+void binary_to_string(std::string& str) {
+}
+
+template <typename... Bytes>
+auto binary_to_string(std::string& str, unsigned char byte, Bytes... bytes) {
+  const auto [h, l] = CharMap::GetInstance().lower(byte);
+  str.push_back(h);
+  str.push_back(l);
+  binary_to_string(str, bytes...);
+}
+}  // namespace internal
+
+template <typename... Bytes>
+auto binary_to_string(Bytes... args) {
+  std::string str;
+  internal::binary_to_string(str, args...);
+  fmt::print("{}\n", str);
+  return str;
+}
+
+TEST_CASE(util::problem_name().data()) {
+  CHECK(binary_to_string(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                         17) ==
+        std::string("0102030405060708090a0b0c0d0e0f1011"));
+}
